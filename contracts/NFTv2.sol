7cf0dc087bf2195bde21eb3ec8ea1430a9c9da41
sergeykhach <sosesallc@gmail.com>
2023-03-23 18:43:53 +0400

    2. das28 arajin mas, pattern proxy upgradeable: Transparent, poxum enq contracty

--- contracts/NFTv2.sol
+++ contracts/NFTv2.sol
@@ -6,30 +6,33 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721URIStorageUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";//es toghy avaleacnu enq vor modificator initializer ogtagorcenq constructory poxaren
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/utils/CountersUpgradeable.sol";
+//import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 
-contract MyToken is Initializable, ERC721Upgradeable, ERC721URIStorageUpgradeable, OwnableUpgradeable
+contract MyTokenV2 is Initializable, ERC721Upgradeable, ERC721URIStorageUpgradeable, OwnableUpgradeable 
 {
     using CountersUpgradeable for CountersUpgradeable.Counter;
 
     CountersUpgradeable.Counter private _tokenIdCounter;
+//erkrord realizaciayi jamanak es funkcian el petq chi pti mi angamlini
+    // function initialize() initializer public {
+    //     __ERC721_init("MyToken", "MTK");
+    //     __ERC721URIStorage_init();
+    //     __Ownable_init();
+    // }
+    //constructor() ERC721("MyToken", "MTK") {}
 
-// constructor() ERC721("MyToken", "MTK") {} //upgradable contractnery chen karogh constructor unenal, dra hamar ogtagorcum enq taki psevdo konstructory initial...
-
-//initializer- modificator a vercvum a verevic vor mi angam kanchenq constructori nman //sarqum u initilize enq anum construtori nman 
-    function initialize() initializer public {
-        __ERC721_init("MyToken", "MTK");
-        __ERC721URIStorage_init();
-        __Ownable_init();
-        //__UUPSUpgradeable_init(); 
-    }//sranq el initialize enq anum vor or vladeleci v momont depolya ustanovit ani
-
-     function safeMint(address to, string memory uri) public onlyOwner {
+    function safeMint(address to, string memory uri) public onlyOwner {
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
         _setTokenURI(tokenId, uri);
+    }
+
+// taki funkcian avelacrel enq arajin contri vra
+    function demo() external pure returns(bool) {
+        return true;
     }
 
     // The following functions are overrides required by Solidity.
@@ -47,5 +50,5 @@ contract MyToken is Initializable, ERC721Upgradeable, ERC721URIStorageUpgradeabl
         return super.tokenURI(tokenId);
     }
 
-   // function _authorizeUpgrade(address newImplementation) internal onlyOwner override {}
+    //function _authorizeUpgrade(address newImplementation) internal onlyOwner override {}
 }
--- test/token-test.ts
+++ test/token-test.ts
@@ -9,7 +9,7 @@ describe("Upgradeable token", function() {
     const NFTFactory = await ethers.getContractFactory("MyToken");//smart contracty gtnumm enq 
     const token = await upgrades.deployProxy(NFTFactory, [], {//deploy enq anum ham proxin(ira het el mycontracty) argery NFT factorin u initialayzi arg-y qani vor datark a mer mot datark array a  
       initializer: 'initialize',
-      //kind: 'uups',//defaulty transparent patterna
+     // kind: 'uups',//defaulty transparent patterna
     }); //tokenin dimeluc dimelu enq proxiin, vori mej 
     await token.deployed(); //arden deploy eghaca
 
@@ -26,13 +26,13 @@ describe("Upgradeable token", function() {
     //asum enq 1 NFT pti lini es ameni ardyunqum
     expect(await token.balanceOf(deployer.address)).to.eq(1);
 
-    /*
-    const NFTFactoryv2 = await ethers.getContractFactory("MyTokenV2");
-    const token2 = await upgrades.upgradeProxy(token.address, NFTFactoryv2);
+    //sa el v2 - hamar
+    const NFTFactoryv2 = await ethers.getContractFactory("MyTokenV2"); //nor versian
+    const token2 = await upgrades.upgradeProxy(token.address, NFTFactoryv2); //ubgrade enq anum nor 
 
-    expect(await token2.balanceOf(deployer.address)).to.eq(1);
-    expect(await token2.demo()).to.be.true;
-    console.log(token.address);
-    console.log(token2.address);*/
+    expect(await token2.balanceOf(deployer.address)).to.eq(1); //stugum enq vor ankax v1-v2 popoxutyunic balanci 1 NFT mnuma
+    expect(await token2.demo()).to.be.true; //en demo funkcian kontracti ashkhatuma
+    console.log(token.address); //hascenery nuyna talu votev nuyn proxii hascena
+    console.log(token2.address);
   });
 });
